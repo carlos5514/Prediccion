3b3fadb550577597b11810647a3407e8dfffcd4a
Carlos <carlos.serrano@cunef.edu>
2020-11-23 18:32:31 +0100

    CP05_COLGATE

---
+++ CP05_COLGATE/CP05_COLGATE.R
@@ -0,0 +1,11 @@
+---
+title: "CP05 - Colgate vs Crest"
+author: "Carlos Serrano Valera"
+date: "23/11/2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
